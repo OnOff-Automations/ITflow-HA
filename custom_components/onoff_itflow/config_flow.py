2f6f68f328fd2bbe4095ee6c34adc26c1e5b665c
autohhacs <hacs@autoh.cloud>
2025-11-25 21:51:28 -0500

    Remove config_entry parameter from options flow

--- custom_components/onoff_itflow/config_flow.py
+++ custom_components/onoff_itflow/config_flow.py
@@ -219,15 +219,11 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry):
         """Get the options flow for this handler."""
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 
 class OptionsFlowHandler(OptionsFlow):
     """Handle options flow for OnOff Automations integration."""
-
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
